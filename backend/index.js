0e1a5eca54f1ed5e4cb7288851c298979c824933
GamBar <GamBar>
2022-04-13 11:37:00 +0300

    credentials

--- backend/index.js
+++ backend/index.js
@@ -17,3 +17,11 @@ app.use((req, res, next) => {
 app.listen(3030, () => {
   console.log('Example app listening on port 3030!');
 });
+
+/*
+
+headers
+key : 70LMZ6nF089Nb0KEyoLww1LFq8mW30qZe3iFpm3JJ0ZJ0UFS
+user: stefano
+
+*/
