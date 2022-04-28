793e2cb2da9ef5b787a44979ebb01126982083d3
TAVnotDove <user-email>
2022-04-28 13:15:00 +0300

    register-data-verification

--- src/views/register.js
+++ src/views/register.js
@@ -9,18 +9,19 @@ const registerTemplate = (submitForm) => html`
         <form class="flex-form" @submit=${submitForm}>
             <div class="flex-form-div">
                 <label>Email</label>
-                <input name="email">
+                <input name="email" placeholder="name@domain.extension">
             </div>
             <div class="flex-form-div">
                 <label>Password</label>
-                <input name="password" type="password">
+                <input name="password" type="password" placeholder="••••••">
             </div>
             <div class="flex-form-div">
                 <label>Repeat Password</label>
-                <input name="repeat-password" type="password">
+                <input name="repeat-password" type="password" placeholder="••••••">
             </div>
             <button type="submit">Submit</button>
             <p class="error-message"></p>
+            <p class="requirements">Password should: <br>-be at least 6 characters long;<br>-have a small letter, capital letter, symbol, number.</p>
         </form>
     </div>
 `
@@ -34,21 +35,36 @@ export function renderRegister(ctx) {
         let password = formData.get('password').trim()
         let repeatPassword = formData.get('repeat-password').trim()
 
+        let emailRegExp = /[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+/gm
+        let passwordRegExp = /^(?=[^A-Z\n]*[A-Z])(?=[^a-z\n]*[a-z])(?=[^0-9\n]*[0-9])(?=[^#?!@$%^&*\n-]*[#?!@$%^&*-]).{6,}$/gm
+        let emailIsValid = email.match(emailRegExp)
+        let passwordIsValid = password.match(passwordRegExp)
+
+        let errorMessageElement = document.querySelector('.error-message')
+
         if (email.length !== 0 && password.length !== 0 && repeatPassword.length !== 0) {
-            if (password === repeatPassword) {
-                let response = await register(email, password)
-                
-                if (response) {
-                    localStorage.setItem('user', JSON.stringify({email, response}))
-                    ctx.page.redirect('/')
+            if (emailIsValid) {
+                if (passwordIsValid) {
+                    if (password === repeatPassword) {
+                        let response = await register(email, password)
+                        
+                        if (response) {
+                            localStorage.setItem('user', JSON.stringify({email, response}))
+                            ctx.page.redirect('/')
+                        } else {
+                            errorMessageElement.textContent = "A user with the same email already exists."
+                        }
+                    } else {
+                        errorMessageElement.textContent = "Passwords don't match."
+                    }
                 } else {
-                    document.querySelector('.error-message').textContent = "A user with the same email already exists."
+                    errorMessageElement.textContent = "Invalid password format."
                 }
             } else {
-                document.querySelector('.error-message').textContent = "Passwords don't match."
+                errorMessageElement.textContent = "Invalid email format."
             }
         } else {
-            document.querySelector('.error-message').textContent = "Please fill in all input fields."
+            errorMessageElement.textContent = "Please fill in all input fields."
         }
     }
 
